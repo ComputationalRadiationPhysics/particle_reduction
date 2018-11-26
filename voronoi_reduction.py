f2ef68d63f4a32e2aeb7087b45c71b893dbf5892
Unknown <Bastrakova.Kseinia@gmail.com>
2018-11-26 10:47:59 +0100

    Add parsing arguments
    hdf file, reduced hdf file, tolerance of momentum, position

---
+++ voronoi_reduction.py
@@ -0,0 +1,22 @@
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="voronoi reduction")
+
+    parser.add_argument("-hdf", metavar='hdf_file', type=str,
+                        help="hdf file without patches")
+
+    parser.add_argument("-hdf_re", metavar='hdf_file_reduction', type=str,
+                        help="reducted hdf file")
+
+    parser.add_argument("-momentum_tol", metavar='tolerance_momentum', type=float,
+                        help="tolerance of momentum")
+
+    parser.add_argument("-momentum_pos", metavar='tolerance_position', type=float,
+                        help="tolerance of position")
+
+    args = parser.parse_args()
+    voronoi_reduction(args.hdf, args.hdf_re, args.momentum_tol, args.momentum_pos)
+
