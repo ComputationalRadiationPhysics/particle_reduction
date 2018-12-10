3147118f3b2607f7d6a2a90f554f72366b308799
Unknown <Bastrakova.Kseinia@gmail.com>
2018-12-10 10:38:32 +0100

    Add simple point class

---
+++ Voronoi_algorithm.py
@@ -0,0 +1,7 @@
+
+class Point:
+    def __init__(self, coords, weight):
+        """A weighted point, coords is array-like, weight is float"""
+        self.coords = coords
+        self.weight = weight
+
