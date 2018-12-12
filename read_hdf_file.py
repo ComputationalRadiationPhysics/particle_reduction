ef6941df74ec779eb368d649fd2f27438d4518f2
Unknown <Bastrakova.Kseinia@gmail.com>
2018-12-12 17:53:09 +0100

    Add decode_name function

--- read_hdf_file.py
+++ read_hdf_file.py
@@ -58,3 +58,11 @@ def get_particles_name(hdf_file):
         particles_name = 'particles'
     return particles_name
 
+
+def decode_name(attribute_name):
+    """ Decode name from binary """
+
+    decoding_name = attribute_name.decode('ascii', errors='ignore')
+    decoding_name = re.sub(r'\W+', '', decoding_name)
+    return decoding_name
+
