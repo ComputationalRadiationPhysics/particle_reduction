877a458f60c6aa38b08c9f4ad3bfe5acae6827d7
Unknown <Bastrakova.Kseinia@gmail.com>
2019-01-07 14:51:21 +0100

    Divided Particles_groups functor to 2 functors
    Particles_functor - collect parent group for particles
    Particles_groups - collect position, momentum group
    and mass value from particle node

--- read_hdf_file.py
+++ read_hdf_file.py
@@ -1,39 +1,42 @@
 import h5py
 import re
 
-class Particles_groups():
+class Particles_functor():
     """ Collect values from datasets in hdf file """
 
-    def __init__(self, particles_name):
+    def __init__(self):
         self.particles_groups = []
         self.positions = []
-        self.name_particles = particles_name
         self.momentum = []
         self.mass = []
+
+    def __call__(self, name, node):
+        if isinstance(node, h5py.Group):
+            if node.name.endswith('position'):
+                self.positions.append(node)
+
+            if node.name.endswith('momentum'):
+                self.momentum.append(node)
+
+            if node.name.endswith('mass'):
+                self.mass = node.attrs['value']
+        return None
+
+
+class Particles_groups():
+    def __init__(self, particles_name):
+
+        self.name_particles = particles_name
+        self.particles_groups = []
 
     def __call__(self, name, node):
         if isinstance(node, h5py.Group):
             name_idx = node.name.find(self.name_particles)
             if name_idx != -1:
                 group_particles_name = node.name[name_idx + len(self.name_particles) + 1:]
-                if group_particles_name.endswith('position'):
-                    self.positions.append(node)
-
-                if group_particles_name.endswith('momentum'):
-                    self.momentum.append(node)
-
-                if group_particles_name.endswith('mass'):
-                    self.mass = node.attrs['value']
+                if group_particles_name.find('/') == -1 and group_particles_name != '':
+                    self.particles_groups.append(node)
         return None
-
-
-class Particles_groups():
-
-
-    def __call__(self, name, node):
-        if isinstance(node, h5py.Group):
-            name_idx = node.name.find(self.name_particles)
-            if name_idx != -1:
 
     def __call__(self, name, node):
         if isinstance(node, h5py.Dataset):
