97477c064faa7c07015572808ca280f1db69f778
Unknown <Bastrakova.Kseinia@gmail.com>
2019-01-07 14:40:14 +0100

    Add voronoi_reduction function

--- reduction_main.py
+++ reduction_main.py
@@ -1,5 +1,23 @@
 import argparse
 
+def voronoi_reduction(hdf_file, hdf_file_reduction, tolerance_momentum, tolerance_position):
+
+    name_hdf_file_reduction = ''
+
+    if hdf_file != '':
+        if os.path.exists(hdf_file):
+            name = hdf_file[:-4]
+            idx_of_name = name.rfind('/')
+            if idx_of_name != -1:
+                name_hdf_file_reduction = hdf_file_reduction + hdf_file[idx_of_name + 1: -6] + 'reduction.h5'
+            else:
+                name_hdf_file_reduction = hdf_file_reduction + hdf_file[:-3] + '.h5'
+
+            tolerances = [tolerance_momentum, tolerance_position]
+           
+        else:
+            print('The .hdf file does not exist')
+
 
 if __name__ == "__main__":
 
@@ -18,5 +36,6 @@ if __name__ == "__main__":
                         help="tolerance of position")
 
     args = parser.parse_args()
+    voronoi_reduction(args.hdf, args.hdf_re, args.momentum_tol, args.momentum_pos)
 
 
