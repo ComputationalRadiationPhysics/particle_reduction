9353701e87c1edfa0ed8e4f1adfcf9a5f163b7da
Unknown <Bastrakova.Kseinia@gmail.com>
2019-01-22 15:47:52 +0100

    Add random_thinning_algorithm to reduction_main

--- reduction_main.py
+++ reduction_main.py
@@ -4,6 +4,7 @@ import argparse
 import Voronoi_algorithm
 import os
 import h5py
+import Random_thinning_algorithm
 
 
 def voronoi_reduction(hdf_file, hdf_file_reduction, tolerance_momentum, tolerance_position):
@@ -45,6 +46,24 @@ def voronoi_algorithm(hdf_file_name, hdf_file_reduction_name, tolerances):
         library_datasets = read_hdf_file.create_library_of_datasets(point_converter.points)
 
         read_hdf_file.write_group_values(hdf_file_reduction, group, library_datasets)
+def random_thinning_algorithm(hdf_file_name, hdf_file_reduction_name, reduction_percent):
+
+    copyfile(hdf_file_name, hdf_file_reduction_name)
+    hdf_file = h5py.File(hdf_file_name, 'a')
+    hdf_file_reduction = h5py.File(hdf_file_reduction_name, 'a')
+    particles_name = read_hdf_file.get_particles_name(hdf_file_reduction)
+    particles_collect = read_hdf_file.ParticlesGroups(particles_name)
+    hdf_file.visititems(particles_collect)
+
+    for group in particles_collect.particles_groups:
+        points = read_hdf_file.read_group_values(group)
+        num_particles, num_particles_offset = read_hdf_file.read_patches_values(group)
+        parameters = Random_thinning_algorithm.RandomThinningAlgorithmParameters(reduction_percent, num_particles, num_particles_offset)
+        algorithm = Random_thinning_algorithm.RandomThinningAlgorithm(parameters)
+        result, num_particles_offset, num_particles = algorithm.run(points)
+        library_datasets = read_hdf_file.create_library_of_datasets(result)
+        read_hdf_file.write_group_values(hdf_file_reduction, group, library_datasets, num_particles_offset,
+                                         num_particles)
 
 
 if __name__ == "__main__":
