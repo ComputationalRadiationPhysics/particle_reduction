1b5246f3797efbbc2d7c3ad4af47fe91580a35de
Unknown <Bastrakova.Kseinia@gmail.com>
2019-03-19 17:50:09 +0100

    Add base merging function
    weights = sum of weights
    vector values = weighted average

---
+++ k_means_clustering_algorithm.py
@@ -0,0 +1,15 @@
+import numpy
+
+
+def merge_points(dimension, vector, weights_vector):
+    """ Merge coordinates into one point """
+
+    values_vector = []
+    sum_weights = numpy.sum(weights_vector, dtype=float)
+    vector = numpy.array(vector)
+
+    for i in range(0, dimension):
+        values_vector.append(numpy.average(vector[:, i], weights=weights_vector))
+
+    return values_vector, sum_weights
+
