0ff38555d89c4498d0fee431cd643172532981d6
Unknown <Bastrakova.Kseinia@gmail.com>
2019-03-25 17:52:40 +0100

    Add Momentum_cell class

--- Vranic_algorithm.py
+++ Vranic_algorithm.py
@@ -10,3 +10,18 @@ class Segment:
         else:
             return False
 
+
+class Momentum_cell:
+
+    def __init__(self):
+        self.segment_x = 0.
+        self.segment_y = 0.
+        self.segment_z = 0.
+
+        self.momentums = []
+
+    def get_idixes(self):
+        self.momentums = np.array(self.momentums)
+        indexes = np.array(self.momentums[:,3], dtype=('int'))
+        return indexes
+
