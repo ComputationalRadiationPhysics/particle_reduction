1229083735d94b975f0ece81c498328e41e033b2
Unknown <Bastrakova.Kseinia@gmail.com>
2019-03-27 15:07:42 +0100

    Add Leveling_Thinning_Algorithm_Parameters class
    parametrs: numParticles, numParticlesOffset -- for patch
    awg_weight_coef - weight coef for choose delete particles

--- Leveling _thinning_algorithm.py
+++ Leveling _thinning_algorithm.py
@@ -1,4 +1,13 @@
 
+
+class Leveling_Thinning_Algorithm_Parameters:
+    def __init__(self, awg_weight_coef, numParticles, numParticlesOffset):
+        """..."""
+        self.awg_weight_coef = awg_weight_coef
+        self.numParticles = numParticles
+        self.numParticlesOffset = numParticlesOffset
+
+
 def get_indices_to_remove(weight_value_of_reduced_particle, weigths):
 
     result = []
