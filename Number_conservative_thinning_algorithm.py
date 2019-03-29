12c3e82ab4b688f4964f15235dba4a43be579ece
Unknown <Bastrakova.Kseinia@gmail.com>
2019-03-29 17:27:12 +0100

    Add recounting weights

--- Number_conservative_thinning_algorithm.py
+++ Number_conservative_thinning_algorithm.py
@@ -3,6 +3,17 @@
 
 
 
+def recount_weights(weights, sample, number_of_k_sample, indexes_to_keep):
+
+    sample = sample.tolist()
+    result_weights = []
+    for i in range(0, len(indexes_to_keep)):
+        size_of_values = sample.count(indexes_to_keep[i])
+        new_weights = size_of_values * weights[indexes_to_keep[i]]/number_of_k_sample
+        result_weights.append(new_weights)
+    return result_weights
+
+
 def get_indices_to_remove(sample, size):
 
     indexes = numpy.array(list(range(0, size)))
