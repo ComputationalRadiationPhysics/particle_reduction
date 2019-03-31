e409de095fcae9ad57f003aa2c7e99eb5e1a9313
Unknown <Bastrakova.Kseinia@gmail.com>
2019-03-31 19:56:31 +0200

    Add some small fixes to Energy conservative algorithm
    fix selection rows 
    add numpy array
    fix range list creation

--- Energy_conservative_thinning_algorithm.py
+++ Energy_conservative_thinning_algorithm.py
@@ -21,7 +21,8 @@ class Energy_conservative_thinning_algorithm:
 
         size = len(data)
         data = numpy.array(data)
-        momentum_values = data[:, 3, 4, 5]
+        momentum_values = data[:, 3:6]
+
         energy_values = calculate_energy_values(momentum_values, mass)
         weigths = numpy.array(weigths)
         sample, sum_weighted_energy = get_random_sample(weigths, energy_values, self.number_of_k_sample)
@@ -56,13 +57,14 @@ def get_random_sample(weights, energy_values, number_of_k_sample):
         weighted_energy.append(weights[i] * energy_values[i])
     sum_weighted_energy = sum(weighted_energy)
     norm_weights = [x / sum_weighted_energy for x in weighted_energy]
-    indexes = list[range(len(weights))]
+    indexes = list(range(len(weights)))
     sample = numpy.random.choice(indexes, size=number_of_k_sample, replace=True, p=norm_weights)
     return sample, sum_weighted_energy
 
 
 def recount_weights(weights, sample, number_of_k_sample, indexes_to_keep, energy, weighted_sum_energy):
 
+    sample = sample.tolist()
     result_weights = []
     for i in range(0, len(indexes_to_keep)):
         size_of_values = sample.count(indexes_to_keep[i])
@@ -73,7 +75,7 @@ def recount_weights(weights, sample, number_of_k_sample, indexes_to_keep, energy
 
 def get_indices_to_remove(sample, size):
 
-    indexes = list[range(len(size))]
+    indexes = numpy.array(list(range(size)))
     indexes_to_keep = [item for item, count in collections.Counter(sample).items() if count > 1]
     select = numpy.in1d(range(indexes.shape[0]), indexes_to_keep)
     indices_to_remove = select[~select]
