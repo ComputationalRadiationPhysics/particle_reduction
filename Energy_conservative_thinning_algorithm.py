5c261ba28644aad0ff236e2bb7c00bafb06cb216
Unknown <Bastrakova.Kseinia@gmail.com>
2019-03-31 18:32:25 +0200

    Add getting indexes to remove

---
+++ Energy_conservative_thinning_algorithm.py
@@ -0,0 +1,9 @@
+
+def get_indices_to_remove(sample, size):
+
+    indexes = list[range(len(size))]
+    indexes_to_keep = [item for item, count in collections.Counter(sample).items() if count > 1]
+    select = numpy.in1d(range(indexes.shape[0]), indexes_to_keep)
+    indices_to_remove = select[~select]
+    return indices_to_remove, indexes_to_keep
+
