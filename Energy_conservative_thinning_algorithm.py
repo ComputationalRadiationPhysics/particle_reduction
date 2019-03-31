5c328317183e81943edc168588e4fbe092760a66
Unknown <Bastrakova.Kseinia@gmail.com>
2019-03-31 18:34:14 +0200

    Add recounting weights

--- Energy_conservative_thinning_algorithm.py
+++ Energy_conservative_thinning_algorithm.py
@@ -1,4 +1,15 @@
 
+
+def recount_weights(weights, sample, number_of_k_sample, indexes_to_keep, energy, weighted_sum_energy):
+
+    result_weights = []
+    for i in range(0, len(indexes_to_keep)):
+        size_of_values = sample.count(indexes_to_keep[i])
+        new_weights = size_of_values * weighted_sum_energy/(number_of_k_sample * weights[indexes_to_keep[i]] * energy[indexes_to_keep[i]])
+        result_weights.append(new_weights)
+    return result_weights
+
+
 def get_indices_to_remove(sample, size):
 
     indexes = list[range(len(size))]
