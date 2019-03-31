ae95aef368a7e4029f8be7c934ca474a07d521bc
Unknown <Bastrakova.Kseinia@gmail.com>
2019-03-31 18:35:07 +0200

    Add getting random sample

--- Energy_conservative_thinning_algorithm.py
+++ Energy_conservative_thinning_algorithm.py
@@ -1,3 +1,21 @@
+import random
+import numpy
+import collections
+import math
+
+
+
+def get_random_sample(weights, energy_values, number_of_k_sample):
+
+    weighted_energy = []
+
+    for i in range(0, len(weights)):
+        weighted_energy.append(weights[i] * energy_values[i])
+    sum_weighted_energy = sum(weighted_energy)
+    norm_weights = [x / sum_weighted_energy for x in weighted_energy]
+    indexes = list[range(len(weights))]
+    sample = numpy.random.choice(indexes, size=number_of_k_sample, replace=True, p=norm_weights)
+    return sample, sum_weighted_energy
 
 
 def recount_weights(weights, sample, number_of_k_sample, indexes_to_keep, energy, weighted_sum_energy):
