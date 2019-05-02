20c5c61b8fa9f4cdd1bc95b72da36422ffc46d25
Unknown <Bastrakova.Kseinia@gmail.com>
2019-05-02 16:10:04 +0200

    Add multiple_runs_number_conservative function

--- helpers/multiple_runs/multiple_runs.py
+++ helpers/multiple_runs/multiple_runs.py
@@ -1,4 +1,20 @@
 import argparse
+import sys
+sys.path.append("../../")
+import reduction_main
+
+
+def multiple_runs_number_conservative(hdf_file_name, hdf_path_result, reduction_percent_start, reduction_percent_end, reduction_percent_step):
+
+    number_of_runs = int((reduction_percent_end -reduction_percent_start)/reduction_percent_step)
+
+    for i in range(0, number_of_runs):
+        ratio_of_deleted_particles = reduction_percent_start + i * reduction_percent_step
+        name_hdf_file_reduction = hdf_path_result + 'number_conservative_reduction_' + str(ratio_of_deleted_particles)[0:3] + '.h5'
+        print('name_hdf_file_reduction  ' + str(name_hdf_file_reduction))
+        reduction_main.number_conservative_thinning_algorithm(hdf_file_name, name_hdf_file_reduction, ratio_of_deleted_particles)
+
+
 
 
 if __name__ == "__main__":
@@ -26,3 +42,7 @@ if __name__ == "__main__":
 
 
     args = parser.parse_args()
+
+    if args.algorithm == 'number_conservative':
+        multiple_runs_number_conservative(args.hdf, args.hdf_re, args.reduction_percent_start,
+                                          args.reduction_percent_end, args.reduction_percent_step)
