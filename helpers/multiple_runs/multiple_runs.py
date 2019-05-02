af652f60069d11a1d44a1b3a0fac33d271d21d37
Unknown <Bastrakova.Kseinia@gmail.com>
2019-05-02 16:20:18 +0200

    Add multiple_runs_k_means_avg function

--- helpers/multiple_runs/multiple_runs.py
+++ helpers/multiple_runs/multiple_runs.py
@@ -48,6 +48,17 @@ def multiple_runs_k_means_clustering(hdf_file_name, hdf_path_result, reduction_p
         reduction_main.k_means_cluster_algorithm(hdf_file_name, name_hdf_file_reduction, ratio_of_deleted_particles)
 
 
+def multiple_runs_k_means_avg(hdf_file_name, hdf_path_result, reduction_percent_start, reduction_percent_end, reduction_percent_step):
+
+    number_of_runs = int((reduction_percent_end -reduction_percent_start)/reduction_percent_step)
+
+    for i in range(0, number_of_runs):
+        ratio_of_deleted_particles = reduction_percent_start + i * reduction_percent_step
+        name_hdf_file_reduction = hdf_path_result + '/k_means_clustering_' + str(ratio_of_deleted_particles)[0:3] + '.h5'
+        print('name_hdf_file_reduction  ' + str(name_hdf_file_reduction))
+        reduction_main.k_means_avg_algorithm(hdf_file_name, name_hdf_file_reduction, ratio_of_deleted_particles)
+
+
 if __name__ == "__main__":
     """ Parse arguments from command line """
 
@@ -88,6 +99,10 @@ if __name__ == "__main__":
 
     elif args.algorithm == 'k_means_clustering':
         multiple_runs_k_means_clustering(args.hdf, args.hdf_re, args.reduction_percent_start,
+                                          args.reduction_percent_end, args.reduction_percent_step)
+
+    elif args.algorithm == 'k_means_avg':
+        multiple_runs_k_means_avg(args.hdf, args.hdf_re, args.reduction_percent_start,
                                           args.reduction_percent_end, args.reduction_percent_step)
 
 
