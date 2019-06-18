665391b9cf4547f53ad0abba84dc59ee24db7ac3
Unknown <Bastrakova.Kseinia@gmail.com>
2019-06-18 14:22:37 +0200

    Number_conservative move sample iteration, add time counting

--- Algorithms/Number_conservative_thinning_algorithm.py
+++ Algorithms/Number_conservative_thinning_algorithm.py
@@ -1,7 +1,7 @@
 import random
 import numpy
 import collections
-
+import time
 
 class Number_conservative_thinning_algorithm_parameters:
     def __init__(self, reduction_percent):
@@ -25,8 +25,10 @@ class Number_conservative_thinning_algorithm:
         weigths = numpy.array(weigths)
         sample = get_random_sample(weigths, number_of_k_sample)
         indices_to_remove, indexes_to_keep = get_indices_to_remove(sample, size)
+        num_indexes_to_keep = numpy.array(indexes_to_keep)[:, 0]
         weights_to_keep = recount_weights(weigths, sample, number_of_k_sample, indexes_to_keep)
-        return data[indexes_to_keep], weights_to_keep
+
+        return data[num_indexes_to_keep], weights_to_keep
 
 
 def get_random_sample(weights, number_of_k_sample):
@@ -40,20 +42,23 @@ def get_random_sample(weights, number_of_k_sample):
 
 def recount_weights(weights, sample, number_of_k_sample, indexes_to_keep):
 
-    sample = sample.tolist()
     result_weights = []
     for i in range(0, len(indexes_to_keep)):
-        size_of_values = sample.count(indexes_to_keep[i])
-        new_weights = size_of_values * weights[indexes_to_keep[i]]/number_of_k_sample
+       # size_of_values = sample.count(indexes_to_keep[i])
+        new_weights = indexes_to_keep[i][1] * weights[indexes_to_keep[i][0]]/number_of_k_sample
         result_weights.append(new_weights)
+
     return result_weights
 
 
 def get_indices_to_remove(sample, size):
 
     indexes = numpy.array(list(range(0, size)))
+
     sample.tolist()
-    indexes_to_keep = [item for item, count in collections.Counter(sample).items() if count > 1]
+    indexes_to_keep = [(item, count) for item, count in collections.Counter(sample).items() if count > 1]
     select = numpy.in1d(range(indexes.shape[0]), indexes_to_keep)
+
     indices_to_remove = select[~select]
+
     return indices_to_remove, indexes_to_keep
