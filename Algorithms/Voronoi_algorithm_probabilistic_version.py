b4907e549aaed0cef1fe2af1f304c33b8c44c8d2
Unknown <Bastrakova.Kseinia@gmail.com>
2019-07-16 12:38:39 +0200

    Add probabilistic algorithm parameters

---
+++ Algorithms/Voronoi_algorithm_probabilistic_version.py
@@ -0,0 +1,10 @@
+
+
+class Voronoi_probabilistic_algorithm_parameters:
+    """"""
+
+    def __init__(self, tolerance, reduction_percent, ratio_left_particles):
+        self.tolerance = tolerance
+        self.reduction_percent = reduction_percent
+        self.ratio_left_particles = ratio_left_particles
+
