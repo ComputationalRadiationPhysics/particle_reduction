efc15a1702ee0d9a12c0cd1c3cf704b9d6eb492c
Unknown <Bastrakova.Kseinia@gmail.com>
2019-07-16 12:40:50 +0200

    Add main algorithm

--- Algorithms/Voronoi_algorithm_probabilistic_version.py
+++ Algorithms/Voronoi_algorithm_probabilistic_version.py
@@ -8,3 +8,15 @@ class Voronoi_probabilistic_algorithm_parameters:
         self.reduction_percent = reduction_percent
         self.ratio_left_particles = ratio_left_particles
 
+
+class Voronoi_probabilistic_algorithm:
+    """Main algorithm. Parameters is Voronoi_probabilistic_algorithm_parameters """
+
+    def __init__(self, parameters):
+        self.parameters = parameters
+        self.dimensions = None
+
+    def _run(self, data, weigths):
+
+        return _merge(data, weigths, self.parameters, self.dimensions)
+
