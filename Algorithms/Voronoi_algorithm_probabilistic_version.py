42be6a7093091b499a099b908872c10a9ec59ea5
Unknown <Bastrakova.Kseinia@gmail.com>
2019-07-16 12:45:57 +0200

    Get coefficient of variance

--- Algorithms/Voronoi_algorithm_probabilistic_version.py
+++ Algorithms/Voronoi_algorithm_probabilistic_version.py
@@ -32,3 +32,23 @@ class _Voronoi_cell:
         self.weights = numpy.array(weigths)
         self.size_of_divide_particles = size_of_divide_particles
         self.expected_number_of_particles = expected_number_of_particles
+
+    def get_coeff_var(self):
+
+        """Get max variance coefficient of Voronoi cell"""
+
+        dimension = len(self.vector[0])
+        avg_values = []
+
+        for i in range(0, dimension):
+            values_dimension = []
+            weights_dimension = []
+            for j in range(0, len(self.vector)):
+                values_dimension.append(self.vector[j][i])
+                weights_dimension.append(self.weights[j])
+
+            std = weighted_std(values_dimension, weights_dimension)
+            avg_values.append(std)
+
+        max_idx, max_avg = get_max_coef(avg_values)
+        return max_idx, max_avg
