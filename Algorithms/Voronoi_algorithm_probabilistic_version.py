c35712a5ff0edd12990b23af728964edd1b790bf
Unknown <Bastrakova.Kseinia@gmail.com>
2019-07-16 12:52:37 +0200

    Add points merging

--- Algorithms/Voronoi_algorithm_probabilistic_version.py
+++ Algorithms/Voronoi_algorithm_probabilistic_version.py
@@ -103,3 +103,16 @@ class _Voronoi_cell:
 
         self.vector, self.weights = merge_points(dimension, self.vector, self.weights)
 
+
+def merge_points(dimension, vector, weights_vector):
+    """ Merge coordinates into one point """
+
+    values_vector = []
+    sum_weights = numpy.sum(weights_vector, dtype=float)
+    vector = numpy.array(vector)
+
+    for i in range(0, dimension):
+        values_vector.append(numpy.average(vector[:, i], weights=weights_vector))
+
+    return values_vector, sum_weights
+
