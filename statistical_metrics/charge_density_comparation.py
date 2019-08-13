420be2f89d94dc3d23556a71d8b3cf9c764936c8
Unknown <Bastrakova.Kseinia@gmail.com>
2019-08-13 11:32:13 +0200

    Add reading density

--- statistical_metrics/charge_density_comparation.py
+++ statistical_metrics/charge_density_comparation.py
@@ -1,6 +1,8 @@
 
 import numpy
 import math
+import argparse
+import csv
 class get_fields():
     """
 
@@ -17,3 +19,28 @@ class get_fields():
             self.fields = node
         return None
 
+
+class Read_density():
+    def __init__(self):
+        self.charge_density = {}
+        self.density = {}
+        self.energy_density = {}
+        self.name_particles = 'fields'
+
+    def get_name_group(self, node_name):
+        name_idx = node_name.find(self.name_particles)
+        group_particles_name = ''
+        if name_idx != -1:
+            group_particles_name = node_name[name_idx + len(self.name_particles) + 1:]
+
+        return group_particles_name
+
+    def __call__(self, name, node):
+        if isinstance(node, h5py.Dataset):
+            name_group = self.get_name_group(node.name)
+            if node.name.endswith('_chargeDensity'):
+                self.charge_density[name_group] = node.value
+            if node.name.endswith('_density'):
+                self.density[name_group] = node.value
+            if node.name.endswith('_energyDensity'):
+                self.energy_density[name_group] = node.value
