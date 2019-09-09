f4b640538c7f83b320598871f6c993f39bd1eabe
KseniaBastrakova <xeinia.baykova@gmail.com>
2019-09-09 17:43:57 +0200

    Add parse command arguments

--- statistical_metrics/one_dimention_KDE_comparation.py
+++ statistical_metrics/one_dimention_KDE_comparation.py
@@ -1,5 +1,7 @@
+import sys
 import csv
 import numpy
+import argparse
 
 
 def write_values_into_csv_file(metric_values, csv_file_name):
@@ -28,3 +30,29 @@ def norm_array01(vector, max_value, min_value):
     return array_norm
 
 
+
+
+
+
+
+
+if __name__ == "__main__":
+
+
+    parser = argparse.ArgumentParser(description="file_comparation")
+
+    parser.add_argument("-first_hdf", metavar='first_hdf', type=str,
+                        help="first hdf file to compare")
+
+    parser.add_argument("-second_hdf", metavar='second_hdf', type=str,
+                        help="second hdf file to compare")
+
+    parser.add_argument("-csv_file", metavar='csv_file', type=str,
+                        help="csv file")
+
+    parser.add_argument("-file_indx", metavar='file_indx', type=str,
+                        help="information about name iteration")
+
+    args = parser.parse_args()
+
+    base_corparation(args.first_hdf, args.second_hdf, args.csv_file, args.file_indx)
